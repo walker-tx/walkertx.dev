6e1d3a3eca6d95d5a9c12fbf071bb130e5c6b1c8
Walker <walker.b.lockard@gmail.com>
2024-09-06 14:49:45 -0500

    update readme and get rid of some unnecessary assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,29 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import remarkDirective from "remark-directive";
 import { admonishmentPlugin } from "./src/lib/remark-plugins/admonitions.mjs";
 import { setDefaultLayout } from "./src/lib/remark-plugins/default-layout.mjs";
 
 export default defineConfig({
+  site: process.env.URL,
   markdown: {
     remarkPlugins: [remarkDirective, admonishmentPlugin, setDefaultLayout],
   },
-  site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  experimental: {
+    env: {
+      schema: {
+        /**
+         * @see https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
+         * */
+        URL: envField.string({
+          context: "server",
+          access: "public",
+          optional: true,
+        }),
+      },
+    },
+  },
 });
